34e5e97d2ecdf0b35de01db9349a1dd8e80083d5
Ansar Myrzagerey <ansardopcloud@gmail.com>
2023-02-08 09:47:49 +0300

    Task A

---
+++ ArrayCreation.java
@@ -0,0 +1,12 @@
+package HW1_Assignment2;
+
+public class ArrayCreation{
+    public static void createArray(int x) {
+        int[] rndarr = new int[x];
+        
+        for (int i = 0; i < x; i++) {
+            int n = (int) (Math.random() * 101);
+            rndarr[i] = n;
+        }
+    }
+}
